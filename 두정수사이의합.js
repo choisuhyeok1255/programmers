279adb1a9ecdff79bd72e64359b1ae7a041ebcf5
choisuhyeok1255 <csh300029@gmail.com>
2021-03-07 00:03:28 +0900

    두 정수 사이의 합

---
+++ 두정수사이의합.js
@@ -0,0 +1,19 @@
+function solution(a, b) {
+  let answer = 0;
+
+  if(a === b) return a;
+
+  if(a > b) {
+    for(let i = b; i <= a; i++){
+      answer += i;
+    }
+    return answer;
+  }else{
+    for(let i = a; i <= b; i++){
+      answer += i;
+    }
+    return answer;
+  }
+}
+
+solution(3, 5);
