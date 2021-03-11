25ca9ed74ed1188fe13a05a561170c8ec28b5c23
choisuhyeok1255 <csh300029@gmail.com>
2021-03-12 00:06:03 +0900

    제일 작은 수 제거하기 완성

--- 제일작은수제거하기.js
+++ 제일작은수제거하기.js
@@ -5,30 +5,9 @@ function solution(arr) {
 
   if(arr.length === 1) return [-1];
 
-  arr.forEach((_, index) => {
-    if(index === arr.length - 1) return min;
-    min = Math.min(arr[index], arr[index + 1]);
-  });
+  min = Math.min(...arr);
 
   answer = arr.filter(num => num !== min);
-
-  console.log(answer);
-
-  // for(let i = 0; i < arr.length; i++){
-  //   if(min > arr[i]) min = arr[i];
-  // }
-
-  // arr.forEach((num, index) => {
-  //   if(min > num) minIndex = index;
-  // });
-
-  // arr.forEach((num) => {
-  //   if(arr[minIndex] !== num) answer.push(num);
-  // });
-
-  // if(answer.length === 0) return [-1];
-
-
 
   return answer;
 }
